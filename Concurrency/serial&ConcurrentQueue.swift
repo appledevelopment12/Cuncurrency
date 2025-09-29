436a755018bdb484bd3bee27316c8e13ff96e81a
Rohit <appledevelopment12@gmail.com>
2025-09-29 12:46:42 +0530

    First

---
+++ Concurrency/serial&ConcurrentQueue.swift
@@ -0,0 +1,101 @@
+//
+//  serial&ConcurrentQueue.swift
+//  Concurrency
+//
+//  Created by Rohit on 18/09/25.
+//
+
+import UIKit
+
+class serial_ConcurrentQueue: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+    //Thread // task
+    //Serial + sync - order
+    //Serial + Async - order
+    //Concurrent + Async - UNORDER
+        
+        
+        
+     //   serial
+        
+    //    func queueTesting(){
+    //        let myQueue = DispatchQueue(label: "YogeshPatel.serial.queue")
+    //
+    //        myQueue.sync {
+    //            print("Task 1 started")
+    //
+    //            for index in 1...5{
+    //                print("\(index) [Task-1] time 5 is \(index * 5)")
+    //            }
+    //            print("Task 1 finished")
+    //        }
+    //        myQueue.sync{
+    //            print("Task 2 started")
+    //
+    //            print("Task 2 finished")
+    //        }
+    //    }
+        
+    //    // concurrent with sync - order maintain
+    //
+    //    func queueTesting(){
+    //        let myQueue = DispatchQueue(label: "YogeshPatel.serial.queue",attributes: .concurrent)
+    //
+    //        myQueue.sync {
+    //            print("Task 1 started")
+    //
+    //            for index in 1...5{
+    //                print("\(index) [Task-1] time 5 is \(index * 5)")
+    //            }
+    //            print("Task 1 finished")
+    //        }
+    //        myQueue.sync{
+    //            print("Task 2 started")
+    //
+    //            print("Task 2 finished")
+    //        }
+    //    }
+    //
+    //    //output :- task 1 start
+    //    //task print
+    //    // task 1 finished
+    //    // task 2 start
+    //    // task 2 finsihed
+
+        
+        
+        
+        // concurrent with async - order not maintain
+       
+    //       func queueTesting(){
+    //           let myQueue = DispatchQueue(label: "YogeshPatel.serial.queue",attributes: .concurrent)
+    //
+    //           myQueue.sync {
+    //               print("Task 1 started")
+    //
+    //               for index in 1...5{
+    //                   print("\(index) [Task-1] time 5 is \(index * 5)")
+    //               }
+    //               print("Task 1 finished")
+    //           }
+    //           myQueue.sync{
+    //               print("Task 2 started")
+    //
+    //               print("Task 2 finished")
+    //           }
+    //       }
+       
+           //output :- task 1 start
+           // task 2 start
+           // task 2 finished
+           // task 1 print
+           // task 1 finsihed
+        
+    }
+
